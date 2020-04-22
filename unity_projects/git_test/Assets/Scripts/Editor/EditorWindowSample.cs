ebce6bb9ab3b2741651b56b57291ecd9f8d3dbff
shion203 <gshion203@gmail.com>
2020-04-22 14:26:31 +0900

    エディタ拡張を試しに行った

---
+++ unity_projects/git_test/Assets/Scripts/Editor/EditorWindowSample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorWindowSample : EditorWindow
+{
+    [MenuItem("Editor/Sample")]
+    private static void Create()
+    {
+        // 生成
+        GetWindow<EditorWindowSample>("サンプル");
+    }
+}
