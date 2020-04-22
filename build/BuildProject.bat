62e2cad914568d7ab31cd407aa50ff86983c4b71
shion203 <gshion203@gmail.com>
2020-04-22 14:11:34 +0900

    コンソールからビルドできるバッチを実装

---
+++ build/BuildProject.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Unity\Hub\Editor\2019.3.9f1\Editor\Unity.exe" -logFile "build.log" -projectPath "C:\git\shion203Test\unity_projects\git_test" -executeMethod BuildClass.Build
---
+++ unity_projects/git_test/Assets/Scripts/Editor/BuildClass.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BuildClass : MonoBehaviour
+{
+    public static void Build()
+    {
+        // ビルド対象シーンリスト
+        string[] sceneList = {
+            "./Assets/scene1.unity",
+        };
+
+        // 実行
+        var errorMessage = BuildPipeline.BuildPlayer(
+                sceneList,                          //!< ビルド対象シーンリスト
+                "C:/test/myUnityProj.exe",   //!< 出力先
+                BuildTarget.StandaloneWindows,      //!< ビルド対象プラットフォーム
+                BuildOptions.Development            //!< ビルドオプション
+        );
+
+        // 結果出力
+        if (!string.IsNullOrEmpty(errorMessage.ToString()))
+            Debug.LogError("[Error!] " + errorMessage.ToString());
+        else
+            Debug.Log("[Success!]");
+    }
+}
